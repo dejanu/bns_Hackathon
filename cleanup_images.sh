2b685128965bdc37cf0e711ed0cece90e31168b0
deal <alexandru.dejanu@systematic.com>
2023-05-02 12:08:52 +0300

    update script

--- cleanup_images.sh
+++ cleanup_images.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-#stop all containers using their id
-docker stop $(docker ps -q) 
-
-# remove all stopped containers
-docker rm -f $(docker ps -aq -f  status=exited) 
-
-# remove all images
-docker rmi -f $(docker images -a -q)
+#stop docker containers
+docker stop flask_app postgres_db
+docker rm flask_app postgres_db
+docker rmi dejanualex/bns_hackathon
