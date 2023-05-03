d7c8dae740218bf26d099d6999165941c5615d61
deal <alexandru.dejanu@systematic.com>
2023-05-03 11:15:27 +0300

    trim stuff

--- backend/app.py
+++ backend/app.py
@@ -8,32 +8,17 @@ from db_stuff.create_db import create_connection, open_cursor, close_cursor, run
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-#connect to postgres database psycopg2 PostgreSQL adapter
+# connect to postgres database psycopg2 PostgreSQL adapter
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 # create the extension
 db = SQLAlchemy()
 # initialize the app with the extension
 db.init_app(app)
 
-# create a new route named database
-@app.route('/database')
-def database():
-    """ create table in the database """
-    # create a connection to the database
-    conn = create_connection(docker=True)
-    run_script('db_stuff/create_table.sql', conn)
-    conn.close()
-    return render_template('database.html')
-
-@app.route('/search')
-def search():
-    """ search object in the database """
-    conn = create_connection(docker=True)
-    with conn.cursor() as cur:
-        cur.execute("SELECT * FROM kubernetes WHERE kobj='pod';")
-        row = cur.fetchone()
-    close_cursor(cur, conn)
-    return render_template('search.html', row=row)
+# create connection and instert data to the database
+conn = create_connection(docker=True)
+run_script('db_stuff/create_table.sql', conn)
+conn.close()
 
 @app.route('/')
 def home():
@@ -43,9 +28,10 @@ def home():
 def submit():
     """ search name object in the database """
     name = request.form['name']
+    db_query = "SELECT * FROM kubernetes WHERE kobj='{}';".format(name)
     conn = create_connection(docker=True)
     with conn.cursor() as cur:
-        cur.execute("SELECT * FROM kubernetes WHERE kobj='pod';")
+        cur.execute(db_query)
         result = cur.fetchone()
     close_cursor(cur, conn)
     return render_template('submit.html', name=name, result=result)
--- backend/db_stuff/create_table.sql
+++ backend/db_stuff/create_table.sql
@@ -6,5 +6,7 @@ CREATE TABLE kubernetes (
   kcommand TEXT NOT NULL
 );
 
--- insert one row into kubernetes table
+-- insert rows into kubernetes table
 INSERT INTO kubernetes (kobj, kdef, kcommand) VALUES ('pod', 'group of one or more containers', 'kubectl get po');
+INSERT INTO kubernetes (kobj, kdef, kcommand) VALUES ('deployment', 'enables declarative updates for Pods and ReplicaSets', 'kubectl get deploy');
+INSERT INTO kubernetes (kobj, kdef, kcommand) VALUES ('statefulset', 'set of pods with consistent identities e.g. Storage,Network', 'kubectl get sts');
